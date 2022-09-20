d934d257e712707d2efe01cabea99eec513e1772
Quang Vinh <vinh.dangquang03@hcmut.edu.vn>
2022-09-21 00:03:15 +0700

    create

---
+++ app.js
@@ -0,0 +1,24 @@
+
+var BMR;
+
+var createBtn = document.querySelector('#btn');
+createBtn.onclick = function () {
+    var name = document.querySelector('input[name=name]').value;
+    var age = document.querySelector('input[name=age]').value;
+    var sex = document.querySelector('input[name=sex]').value;
+    var weight = document.querySelector('input[name=weight]').value;
+    var height = document.querySelector('input[name=height]').value;
+
+    var calo = document.querySelector('#calo');
+    
+        if (sex == "male") {
+            BMR = (66 + (13.7*weight) + (5*height) - (6.8*age));
+        }
+        else BMR = (655 +(9.6*weight) + (1.8*height) - (4.7*age));
+    console.log(BMR);
+    var BMRrender = document.querySelector("#calo");
+    var htmls = `<li>
+             ${BMR}
+             </li>`
+    BMRrender.innerHTML = htmls;
+}
